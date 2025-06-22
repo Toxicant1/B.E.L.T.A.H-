2c19763860acb1a1403eb88759349bd9b34558f4
Toxicant1 <isaac0maina@gmail.com>
2025-06-22 04:21:51 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+
+module.exports = {
+  botName: 'B.E.L.T.A.H',
+  ownerName: 'Ishaq Ibrahim',
+  ownerNumber: '+254741819582',
+  sessionName: 'session',
+  language: 'kiswahili',
+  public: true,
+  prefix: ['.', '/', '!'],  // Bot command prefixes
+  aiStack: 'ChatGPT × CrewDrew × Gminae'
+};
